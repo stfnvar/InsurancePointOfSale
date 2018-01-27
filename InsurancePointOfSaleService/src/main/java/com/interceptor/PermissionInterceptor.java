a557174e14ba34af1b9ddc647fe0efe849e23c47
nsajic <nsajic@NB105.execom.co.yu>
2018-01-27 01:14:09 +0100

    Added logging.

--- InsurancePointOfSaleService/src/main/java/com/interceptor/PermissionInterceptor.java
+++ InsurancePointOfSaleService/src/main/java/com/interceptor/PermissionInterceptor.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.EnableMBeanExport;
@@ -23,6 +25,8 @@ import com.service.UserRolesService;
 @EnableConfigurationProperties
 public class PermissionInterceptor extends HandlerInterceptorAdapter {
 
+	private static final Logger log = LoggerFactory.getLogger(PermissionInterceptor.class);
+	
 	@Autowired
 	private UserRolesService userRolesService;
 	
@@ -40,13 +44,16 @@ public class PermissionInterceptor extends HandlerInterceptorAdapter {
 			for(String s : kat.getAccount().getRoles()){
 				ArrayList<String> lista = userRolesService.getPrivilegesForRole(s.replaceFirst("ROLE_", ""));
 				if(lista.contains(permission)){
+					log.error("ACCES GRANTED FOR USER: [{}], METHOD TYPE: [{}] ON PATH: [{}].", kat.getAccount().getPrincipal().getName(), request.getMethod(), request.getRequestURI());
 					return true;
 				}
 			}
 			
+			log.error("ACCES DENIED FOR USER: [{}], METHOD TYPE: [{}] ON PATH: [{}], REASON: [UNAUTHORIZED REQUES].",kat.getAccount().getPrincipal().getName(), request.getMethod(), request.getRequestURI());
 			response.sendError(401, "Unauthorized request");
 			return true;
 		}
+		log.error("ACCES DENIED: [USER NOT LOGGED IN], METHOD TYPE [{}] ON PATH: [{}].", request.getMethod(), request.getRequestURI());
 		return true;
 	}
 	
